bd568e6d5ecd8d1fd4cab8f5d43eb84a6d5ac176
ago <ago@kayac.com>
2013-03-06 05:22:37 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+view source uft8
+=======
+
+License
+-------
+
+	@license view source uft8 chrome extension
+	Copyright (C) 2013 @kyo_ago
+	License: MIT
---
+++ background.js
@@ -0,0 +1,53 @@
+function headersReceivedListener (evn) {
+	var  results = [];
+	for (var i in evn.responseHeaders) {
+		var header = evn.responseHeaders[i];
+		results.push(header);
+		if (!header.name || header.name.toLowerCase() !== 'content-type') {
+			continue;
+		}
+		if (header.value.match(/charset/)) {
+			continue;
+		}
+		header.value += '; charset=UTF8';
+	}
+	return {
+		'responseHeaders' : results
+	};
+}
+function completedListener () {
+console.debug(111);
+	chrome.webRequest.onHeadersReceived.removeListener(
+		headersReceivedListener,
+		{ 'urls' : ['*://*/*.css', '*://*/*.js'] },
+		['blocking', 'responseHeaders']
+	);
+	setTimeout(function () {
+		chrome.webNavigation.onCompleted.removeListener(
+			completedListener,
+			urlFilter
+		);
+	});
+}
+var urlFilter = {
+	'url' : [
+		{
+			'pathSuffix' : '.css'
+		},
+		{
+			'pathSuffix' : '.js'
+		},
+	]
+};
+chrome.webNavigation.onBeforeNavigate.addListener(function (evn) {
+	chrome.webRequest.onHeadersReceived.addListener(
+		headersReceivedListener,
+		{ 'urls' : ['*://*/*.css', '*://*/*.js'] },
+		['blocking', 'responseHeaders']
+	);
+	chrome.webNavigation.onCompleted.addListener(
+		completedListener,
+		urlFilter
+	);
+}, urlFilter);
+
